8e9b47250e452c7e7fcf85e900e0a38a348af3fb
Rostyslav Proskuriakov <rostyslav.proskuriakov@MacBook-Air-Evelina.local>
2019-05-27 19:16:26 +0300

    CARD-2449: Assertion Trait

--- src/Security/AuthorizationContextAwareTrait.php
+++ src/Security/AuthorizationContextAwareTrait.php
@@ -4,15 +4,18 @@ namespace Graphael\Security;
 
 use Graphael\Server;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 trait AuthorizationContextAwareTrait
 {
-    private function isGranted(array $context, array $attributes, $subject = null): bool
+    private function assertGranted(array $context, array $attributes, $subject = null): void
     {
         $checker = $context[Server::CONTEXT_AUTHORIZATION_KEY];
 
         assert($checker instanceof AuthorizationCheckerInterface);
 
-        return $checker->isGranted($attributes, $subject);
+        if (!$checker->isGranted($attributes, $subject)) {
+            throw new AccessDeniedException();
+        }
     }
 }
